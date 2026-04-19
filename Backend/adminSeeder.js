fa72ad6d5c6d89a8c43daff6df45d14b5c0521ad
Areej atif <139440481+AreejAtif23@users.noreply.github.com>
2026-04-19 16:23:13 +0500

    Update adminSeeder.js

--- Backend/adminSeeder.js
+++ Backend/adminSeeder.js
@@ -42,50 +42,6 @@ createAdmin();
 
 
 
-// import 'dotenv/config';
-// import mongoose from 'mongoose';
-// import bcrypt from 'bcryptjs';
-// import UserModel from './models/User.js';
-
-
-// async function createAdmin() {
-
-//     try {
-//        await mongoose.connect(process.env.MONGO_URI)
-//        console.log("Database connected")
-
-//        // check if admin already exists
-//        var adminExists =  await UserModel.findOne({email: process.env.ADMIN_EMAIL, role: "admin"})
-
-//        if (adminExists) {
-//         console.log("Admin Already Exists")
-//         process.exit(0)
-//        }
-
-
-//        // create the admin account
-//        var salt = await bcrypt.genSalt(10)
-//        var encryptedPassword = await bcrypt.hash(process.env.ADMIN_PASS, salt)
-      
-//        await UserModel.create({
-//         name: "System Admin",
-//         email: process.env.ADMIN_EMAIL,
-//         password: encryptedPassword,
-//         role: "admin"  
-//        })
-
-//        console.log("ADMIN ACCOUNT CREATED")
-
-//     } catch (error) {
-//        console.error("Error Creating Admin Account") 
-//        console.error(error)
-//     }
-    
-// }
-
-// createAdmin()
-
-
 
 
 
